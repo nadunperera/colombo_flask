d2a6e22adab79c89ce6859811c0087803aa7cdb1
Nadun Perera <nadun.perera@me.com>
2018-09-09 00:38:49 +1000

    cleaned app.py file a bit more

--- user_service/app.py
+++ user_service/app.py
@@ -3,7 +3,7 @@ from service.users.restplus import api
 from service.users.routes import ns as user_namespace
 import settings
 
-app = Flask(__name__)
+flask_app = Flask(__name__)
 
 def configure_app(flask_app):
     flask_app.config['SERVER_NAME'] = settings.FLASK_SERVER_NAME
@@ -13,18 +13,16 @@ def configure_app(flask_app):
     flask_app.config['ERROR_404_HELP'] = settings.RESTPLUS_ERROR_404_HELP
 
 def initialize_app(flask_app):
-    configure_app(app)
+    #configure_app(app)
     blueprint = Blueprint('api', __name__, url_prefix='/api')
     api.init_app(blueprint)
     api.namespaces.pop(0) #this is to remove default namespace from swagger doc
     api.add_namespace(user_namespace)
-    app.register_blueprint(blueprint)
-
-    return app
+    flask_app.register_blueprint(blueprint)
 
 def main():
-    initialize_app(app)
-    app.run(debug=settings.FLASK_DEBUG)
+    initialize_app(flask_app)
+    flask_app.run(debug=settings.FLASK_DEBUG)
 
 
 if __name__ == "__main__":
